09e2119dcfdc2103ce6f06271986f7253aff4c1b
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-05 23:35:54 +0100

    Parent & Children Pages

--- wp-content/themes/webdevspark/page.php
+++ wp-content/themes/webdevspark/page.php
@@ -15,17 +15,27 @@ while (have_posts()) {
 </div>
 
 <div class="container container--narrow page-section">
+  <?php
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) { ?>
   <div class="metabox metabox--position-up metabox--with-home-link">
     <p>
-      <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+      <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>">
+        <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?>
+      </a>
+      <span class="metabox__main"><?php echo the_title() ?>
+      </span>
     </p>
   </div>
+  <?php } ?>
+
+
 
   <div class="page-links">
-    <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <h2 class="page-links__title"><a href="<?php echo site_url('about-us') ?>">About Us</a></h2>
     <ul class="min-list">
-      <li class="current_page_item"><a href="#">Our History</a></li>
-      <li><a href="#">Our Goals</a></li>
+      <li class="current_page_item"><a href="<?php echo site_url('our-history') ?>">Our History</a></li>
+      <li><a href="<?php echo site_url('our-goals') ?>">Our Goals</a></li>
     </ul>
   </div>
 
