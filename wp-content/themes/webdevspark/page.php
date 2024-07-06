022b094ba8c1be427ffb23e0a5539e61407549a2
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-06 01:07:52 +0100

    Menu of Child Page Links

--- wp-content/themes/webdevspark/page.php
+++ wp-content/themes/webdevspark/page.php
@@ -17,6 +17,7 @@ while (have_posts()) {
 <div class="container container--narrow page-section">
   <?php
     $theParent = wp_get_post_parent_id(get_the_ID());
+
     if ($theParent) { ?>
   <div class="metabox metabox--position-up metabox--with-home-link">
     <p>
@@ -30,14 +31,37 @@ while (have_posts()) {
   <?php } ?>
 
 
+  <?php
+    // Retrieve an array of child pages of the current post
+    $childPageData = get_pages([
+      'child_of' => get_the_ID()
+    ]);
 
+    // Check if the current post has a parent or if it has child pages
+    if ($theParent || $childPageData) { ?>
   <div class="page-links">
-    <h2 class="page-links__title"><a href="<?php echo site_url('about-us') ?>">About Us</a></h2>
+    <h2 class="page-links__title">
+      <a href="<?php echo site_url('about-us') ?>"><?php echo get_the_title($theParent) ?></a>
+    </h2>
     <ul class="min-list">
-      <li class="current_page_item"><a href="<?php echo site_url('our-history') ?>">Our History</a></li>
-      <li><a href="<?php echo site_url('our-goals') ?>">Our Goals</a></li>
+      <?php
+          // Determine the ID of the post whose children should be listed
+          if ($theParent) {
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
+
+          // List child pages of the determined post
+          wp_list_pages([
+            'title_li' => null,
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order'
+          ])
+          ?>
     </ul>
   </div>
+  <?php } ?>
 
   <div class="generic-content">
     <?php the_content() ?>
