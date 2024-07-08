506cf3dd9e40188cede4f0fddff59edf86ff98fe
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-08 13:48:27 +0100

    Move the search page HTML back to Search.js

--- wp-content/themes/webdevspark/src/modules/Search.js
+++ wp-content/themes/webdevspark/src/modules/Search.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 class Search {
   // 1. Describe and initiate our object
   constructor() {
+    this.addSearchHTML();
+
     this.openButtons = document.querySelectorAll(".js-search-trigger");
     this.closeButton = document.querySelector(".search-overlay .search-overlay__close");
     this.searchOverlay = document.querySelector(".search-overlay");
@@ -33,6 +35,23 @@ class Search {
   }
 
   // 3. methods
+  addSearchHTML() {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" id="search-term" placeholder="What are you looking for?">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="container">
+          <div id="search-overlay__results"></div>
+        </div>
+      </div>
+      `)
+  }
+
   openOverlay() {
     this.searchOverlay.classList.add('search-overlay--active');
     document.body.classList.add('body-no-scroll');
@@ -46,6 +65,7 @@ class Search {
   keyPressDispatcher(e) {
     const { keyCode } = e
 
+    // Key 'S'
     if (keyCode === 83 && !this.isOverlayOpen && document.activeElement.tagName !== "INPUT" && document.activeElement.tagName !== "TEXTAREA") {
       this.openOverlay();
       this.isOverlayOpen = true;
@@ -53,6 +73,7 @@ class Search {
       setTimeout(() => this.focusSearchField(), 300);
     }
 
+    // Key 'Esc'
     if (keyCode === 27 && this.isOverlayOpen) {
       this.closeOverlay();
       this.isOverlayOpen = false;
@@ -109,7 +130,7 @@ class Search {
 
   async getResults() {
     const searchValue = this.searchField.value;
-    const url = `${siteConfig.root_url}/wp-json/wp/v2/posts?search=${ searchValue }`;
+    const url = `${ siteConfig.root_url }/wp-json/wp/v2/posts?search=${ searchValue }`;
 
     try {
       const response = await axios.get(url);
