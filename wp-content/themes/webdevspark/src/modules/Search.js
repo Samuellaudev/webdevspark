16cc38f58680c904704e933ef01a13f958188fe7
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-08 11:50:49 +0100

    Keyboard Events in JavaScript

--- wp-content/themes/webdevspark/src/modules/Search.js
+++ wp-content/themes/webdevspark/src/modules/Search.js
@@ -5,6 +5,9 @@ class Search {
     this.closeButton = document.querySelector(".search-overlay .search-overlay__close");
     this.searchOverlay = document.querySelector(".search-overlay");
     this.events();
+
+    // Related to keypress events
+    this.isOverlayOpen = false;
   }
 
   // 2. events
@@ -13,15 +16,32 @@ class Search {
       btn.addEventListener("click", () => this.openOverlay());
     })
     this.closeButton.addEventListener("click", () => this.closeOverlay());
+    document.addEventListener('keydown', e => this.keyPressDispatcher(e))
   }
 
   // 3. methods
   openOverlay() {
     this.searchOverlay.classList.add('search-overlay--active');
+    document.body.classList.add('body-no-scroll');
   }
 
   closeOverlay() {
     this.searchOverlay.classList.remove('search-overlay--active');
+    document.body.classList.remove('body-no-scroll');
+  }
+
+  keyPressDispatcher(e) {
+    const { keyCode } = e
+
+    if (keyCode === 83 && !this.isOverlayOpen && document.activeElement.tagName !== "INPUT" && document.activeElement.tagName !== "TEXTAREA") {
+      this.openOverlay();
+      this.isOverlayOpen = true;
+    }
+
+    if (keyCode === 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+      this.isOverlayOpen = false;
+    }
   }
 }
 
