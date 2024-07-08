598a144d8f445d20db76a62572797f5e7fd86320
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-08 16:02:52 +0100

    Create Our Own Raw JSON Data (WP_Query / get_posts)

--- wp-content/themes/webdevspark/inc/rest_api_route/search-route.php
+++ wp-content/themes/webdevspark/inc/rest_api_route/search-route.php
@@ -8,7 +8,21 @@ function universityRegisterSearch() {
 }
 
 function universitySearchResults() {
-  return 'test route';
+  $professors = get_posts([
+    'post_type' => 'professor',
+    'posts_per_page' => -1
+  ]);
+
+  $professorResults = [];
+
+  foreach ($professors as $professor) {
+    $professorResults[] = [
+      'title' => $professor->post_title,
+      'permalink' => get_permalink($professor),
+    ];
+  };
+
+  return $professorResults;
 }
 
 add_action('rest_api_init', 'universityRegisterSearch');
