859c71ca076e00a378748b7993d33d2f24415e00
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-10 22:49:46 +0100

    Delete Posts with the REST API; Start Edit Post functionality

--- wp-content/themes/webdevspark/src/modules/MyNotes.js
+++ wp-content/themes/webdevspark/src/modules/MyNotes.js
@@ -3,17 +3,27 @@ import axios from "axios";
 class MyNotes {
   constructor() {
     this.deleteNoteButton = document.querySelectorAll(".delete-note");
+    this.editNoteButton = document.querySelectorAll(".edit-note");
     this.events();
   }
 
   events() {
-    this.deleteNoteButton.forEach(btn => {
-      btn.addEventListener("click", () => this.deleteNote())
-    })
+    if (this.deleteNoteButton) {
+      this.deleteNoteButton.forEach(btn => {
+        btn.addEventListener('click', (e) => this.deleteNote(e))
+      });
+    }
+    if (this.editNoteButton) {
+      this.editNoteButton.forEach(btn => {
+        btn.addEventListener('click', (e) => this.editNote(e))
+      })
+    }
   }
 
-  async deleteNote() {
-    const url = `${ siteConfig.root_url }/wp-json/wp/v2/note/110`;
+  async deleteNote(e) {
+    const thisNote = e.target.closest('li');
+    const noteId = thisNote.getAttribute('data-id')
+    const url = `${ siteConfig.root_url }/wp-json/wp/v2/note/${ noteId }`;
 
     try {
       const response = await axios.delete(url, {
@@ -22,10 +32,29 @@ class MyNotes {
         }
       })
 
-      console.log('response', response.data);
+      // Add fade-out animation and remove note after delay
+      thisNote.style.height = `${ thisNote.offsetHeight }px`;
+      setTimeout(function () {
+        thisNote.classList.add("fade-out")
+      }, 50)
+      setTimeout(function () {
+        thisNote.remove()
+      }, 500)
     } catch (error) {
       console.log(error);
     }
+  }
+
+  async editNote(e) {
+    const thisNote = e.target.closest('li');
+    thisNote.querySelectorAll('.note-title-field, .note-body-field')
+      .forEach(field => {
+        field.removeAttribute('readonly')
+        field.classList.add('note-active-field')
+      })
+
+    const updateButton = thisNote.querySelector('.update-note')
+    updateButton.classList.add('update-note--visible')
   }
 }
 
