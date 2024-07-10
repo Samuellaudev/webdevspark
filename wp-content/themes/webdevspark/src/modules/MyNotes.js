a4fa5e633d4ee245eb3a44194b2c834c50f9598e
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-10 18:52:48 +0100

    "My Notes" Front-end (Part 2)

--- wp-content/themes/webdevspark/src/modules/MyNotes.js
+++ wp-content/themes/webdevspark/src/modules/MyNotes.js
@@ -12,8 +12,20 @@ class MyNotes {
     })
   }
 
-  deleteNote() {
-    alert('are you sure you want to delete?')
+  async deleteNote() {
+    const url = `${ siteConfig.root_url }/wp-json/wp/v2/note/110`;
+
+    try {
+      const response = await axios.delete(url, {
+        headers: {
+          'X-WP-Nonce': siteConfig.nonce
+        }
+      })
+
+      console.log('response', response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
