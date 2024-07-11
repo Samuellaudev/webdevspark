5755dfc64ca5f3b4a4f8341303f839921f0bbb4f
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-11 14:11:12 +0100

    Adjust code style in single-professor page

--- wp-content/themes/webdevspark/single-professor.php
+++ wp-content/themes/webdevspark/single-professor.php
@@ -6,35 +6,35 @@ pageBanner();
 while (have_posts()) {
   the_post();
 ?>
-  <div class="container container--narrow page-section">
-    <div class="generic-content">
-      <div class="row group">
-        <div class="one-third">
-          <?php the_post_thumbnail('professorLandscape'); ?>
-        </div>
-        <div class="two-thirds">
-          <?php the_content() ?>
-        </div>
+<div class="container container--narrow page-section">
+  <div class="generic-content">
+    <div class="row group">
+      <div class="one-third">
+        <?php the_post_thumbnail('professorLandscape'); ?>
+      </div>
+      <div class="two-thirds">
+        <?php the_content() ?>
       </div>
     </div>
+  </div>
 
-    <?php
+  <?php
     $relatedPrograms = get_field('related_programs');
 
     if ($relatedPrograms) { ?>
-      <hr class="section-break">
-      <h2 class="headline headline--medium">Subject(s) Taught</h2>
-      <ul class="link-list min-list">
-        <?php foreach ($relatedPrograms as $program) : ?>
-          <li>
-            <a href="<?= get_the_permalink($program) ?>">
-              <?= get_the_title($program) ?>
-            </a>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    <?php } ?>
-  </div>
+  <hr class="section-break">
+  <h2 class="headline headline--medium">Subject(s) Taught</h2>
+  <ul class="link-list min-list">
+    <?php foreach ($relatedPrograms as $program) : ?>
+    <li>
+      <a href="<?= get_the_permalink($program) ?>">
+        <?= get_the_title($program) ?>
+      </a>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+  <?php } ?>
+</div>
 
 <?php
 }
