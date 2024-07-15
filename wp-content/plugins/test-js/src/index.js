3141dbfb54d9f9753b5e34a426a901965e032ff0
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-07-15 13:37:59 +0100

    Setting Up The Correct Answer

--- wp-content/plugins/test-js/src/index.js
+++ wp-content/plugins/test-js/src/index.js
@@ -1,13 +1,37 @@
 import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from '@wordpress/components'
 import './index.scss'
 
+(() => {
+  let locked = false
+
+  wp.data.subscribe(() => {
+    const results = wp.data.select('core/block-editor').getBlocks().filter((block) => {
+      const checkBlockName = block.name === 'js-plugin/test-js-plugin'
+      const checkIfCorrectAnswer = block.attributes.correctAnswer === undefined
+
+      return checkBlockName && checkIfCorrectAnswer
+    })
+
+    if (results.length && locked === false) { 
+      locked = true
+      wp.data.dispatch('core/editor').lockPostSaving('noanswer')
+    }
+
+    if (!results.length && locked) { 
+      locked = false
+      wp.data.dispatch('core/editor').unlockPostSaving('noanswer')
+    }
+  })
+})()
+
 wp.blocks.registerBlockType('js-plugin/test-js-plugin', {
   title: 'Test JS Plugin',
   icon: 'smiley',
   category: 'common',
   attributes: {
     question: { type: "string" },
-    answers: { type: "array", default: ["red", "green", "blue"] }
+    answers: { type: "array", default: ["redd", "green", "blue"] },
+    correctAnswer: { type: 'number', default: undefined }
   },
   edit: EditComponent,
   save: ({ attributes }) => {
@@ -35,10 +59,17 @@ function EditComponent({ attributes, setAttributes }) {
     setAttributes({ answers: [...attributes.answers, ''] })
   }
 
-  const deleteAnswer = (answer) => {
-    console.log(answer);
+  const deleteAnswer = (answer, indexToDelete) => { 
     const filteredAnswers = [...attributes.answers].filter(item => item !== answer)
     setAttributes({ answers: filteredAnswers })
+    
+    if (indexToDelete === attributes.correctAnswer) { 
+      setAttributes({ correctAnswer: undefined })
+    }
+  }
+
+  const markAsCorrect = (index) => {
+    setAttributes({ correctAnswer: index })
   }
 
   return (
@@ -56,18 +87,21 @@ function EditComponent({ attributes, setAttributes }) {
               <TextControl
                 autoFocus={ answer === '' }
                 value={ answer }
-                onChange={ (newValue) => updateAnswer(newValue, index) } 
+                onChange={ (newValue) => updateAnswer(newValue, index) }
               />
             </FlexBlock>
-            <Button></Button>
             <FlexItem>
-              <Icon className="mark-as-correct" icon='star-empty' />
+              <Button
+                onClick={ () => markAsCorrect(index) }
+              >
+                <Icon className="mark-as-correct" icon={ attributes.correctAnswer == index ? 'star-filled' : 'star-empty' } />
+              </Button>
             </FlexItem>
             <FlexItem>
               <Button
                 isLink
                 className="attention-delete"
-                onClick={ () => deleteAnswer(answer) }
+                onClick={ () => deleteAnswer(answer, index) }
               >
                 Delete
               </Button>
