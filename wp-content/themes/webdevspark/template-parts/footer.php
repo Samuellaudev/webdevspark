75ae13acf05b485d6a6f3a45d75738f7075f2a18
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-08-01 14:08:56 +0100

    Finished modifiying the footer

--- wp-content/themes/webdevspark/template-parts/footer.php
+++ wp-content/themes/webdevspark/template-parts/footer.php
@@ -1,11 +1,31 @@
+<?php
+$socialLinks = [
+  [
+    'class' => 'social-icon-github',
+    'href' => 'https://github.com/samuellaudev',
+    'src' => '/images/svg/icon-github.svg'
+  ],
+  [
+    'class' => 'social-icon-linkedin',
+    'href' => 'https://www.linkedin.com/in/samuel-cf-lau/',
+    'src' => '/images/svg/icon-linkedin.svg'
+  ],
+  [
+    'class' => 'social-icon-instagram',
+    'href' => 'https://www.instagram.com/samuel_cf_lau/',
+    'src' => '/images/svg/icon-instagram.svg'
+  ]
+]
+?>
+
+
 <footer class="site-footer">
   <div class="site-footer__inner container container--narrow">
     <div class="group">
       <div class="site-footer__col-one">
         <h1 class="school-logo-text school-logo-text--alt-color">
-          <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
+          <a href="<?php echo site_url() ?>"><strong>WebDev</strong>Spark</a>
         </h1>
-        <p><a class="site-footer__link" href="#">555.555.5555</a></p>
       </div>
 
       <div class="site-footer__col-two-three-group">
@@ -19,7 +39,7 @@
         </div>
 
         <div class="site-footer__col-three">
-          <h3 class="headline headline--small">Learn</h3>
+          <h3 class="headline headline--small">Get in Touch</h3>
           <nav class="nav-list">
             <?php wp_nav_menu([
               'theme_location' => 'footerLocationTwo'
@@ -29,24 +49,17 @@
       </div>
 
       <div class="site-footer__col-four">
-        <h3 class="headline headline--small">Connect With Us</h3>
+        <h3 class="headline headline--small">Connect</h3>
         <nav>
-          <ul class="min-list social-icons-list group">
-            <li>
-              <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            </li>
+          <ul class="min-list social-icons-list group py-2">
+            <?php foreach ($socialLinks as $item) : ?>
+              <li>
+                <a class="<?php echo $item['class'] ?>" href="<?php echo $item['href'] ?>">
+                  <img src="<?php echo get_theme_file_uri($item['src']) ?>" alt='GitHub' class="w-5">
+                </a>
+              </li>
+            <?php endforeach; ?>
+
           </ul>
         </nav>
       </div>
