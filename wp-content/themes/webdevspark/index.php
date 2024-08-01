757a922219fa5220ce4af20f3c1e0a7761292418
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-08-01 22:01:08 +0100

    Modified css and layout of blog page

--- wp-content/themes/webdevspark/index.php
+++ wp-content/themes/webdevspark/index.php
@@ -2,31 +2,47 @@
 get_template_part('template-parts/header');
 
 $args = [
-  'title' => 'Welcome to our blog!',
-  'subtitle' => 'Keep up with our latest news'
+  'title' => 'Everything about web development',
+  'subtitle' => 'Recent Updates and Stories'
 ];
 pageBanner($args);
+
+$counter = 0;
 ?>
 <div class="container container--narrow page-section">
-  <?php while (have_posts()) { ?>
-    <?php the_post(); ?>
+  <?php while (have_posts()) :
+    the_post();
+    $counter++;
+
+    if ($counter % 2 === 0) {
+      echo '<div class="event-post">';
+    } else {
+      echo '<div class="odd-post">';
+    }
+  ?>
     <div class="post-item">
       <h2 class="headline headline--medium headline--post-title">
         <a href="<?= get_permalink() ?>"><?php the_title() ?></a>
       </h2>
       <div class="metabox">
-        <p>Posted by <?php the_author_posts_link() ?>
+        <p class="inner-content">
+          Posted by <span class="hover:cursor-pointer"><?php the_author_posts_link() ?></span>
           on <?php the_time('M j, Y') ?>
-          in <?= get_the_category_list(', ') ?>
+          in <span class="hover:cursor-pointer"><?= get_the_category_list(', ') ?></span>
         </p>
       </div>
-      <div class="generic-content">
+      <div class="generic-content flex flex-col space-y-4">
         <?php the_excerpt() ?>
-        <p><a class="btn btn--blue" href="<?php the_permalink() ?>">Continue reading &raquo;</a></p>
+        <p class="btn-redirect">
+          <a class="btn bg-primary-400 hover:bg-primary-500" href="<?php the_permalink() ?>">Continue reading &raquo;</a>
+        </p>
       </div>
     </div>
-  <?php } ?>
-  <?php echo paginate_links(); ?>
+  <?php
+    echo '</div>';
+  endwhile;
+  echo paginate_links();
+  ?>
 </div>
 
 <?php get_template_part('template-parts/footer'); ?>
