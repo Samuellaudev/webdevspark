f1f5a813628ffa60a20418995c08ec9c19b76e7a
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-08-08 12:23:53 +0100

    Removed unused test plugins

--- wp-content/plugins/word-filter/inc/filter-logic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function applyFilterLogic($content) {
-  $badWords = explode(',', get_option('plugin_words_to_filter'));
-  $badWordsTrimmed = array_filter(array_map('trim', $badWords));
-  $replacementText = esc_html(get_option('replacement_text', '****'));
-
-  return str_replace($badWordsTrimmed, $replacementText, $content);
-}
