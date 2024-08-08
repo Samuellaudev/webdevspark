94faba860eb12a7405620430a279f2f794ee5ac4
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-08-08 12:42:03 +0100

    Removed unused js files and custom post types

--- wp-content/themes/webdevspark/src/index.js
+++ wp-content/themes/webdevspark/src/index.js
@@ -3,11 +3,7 @@ import "../css/style.scss"
 // Our modules / classes
 import MobileMenu from "./modules/MobileMenu"
 import Search from "./modules/Search"
-import MyNotes from "./modules/MyNotes"
-import Like from "./modules/Like"
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const search = new Search()
-const myNotes = new MyNotes()
-const like = new Like()
